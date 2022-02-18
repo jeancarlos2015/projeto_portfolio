97f5ff3997c6cde3e272ac51a88ff4d1b6d0da38
Jean Carlos Penas <jeancarlospenas25@gmail.com>
2022-02-18 17:35:46 -0300

    Update section2.blade.php

--- resources/views/componentes/tema1/section2.blade.php
+++ resources/views/componentes/tema1/section2.blade.php
@@ -6,7 +6,7 @@
                 <div class="service-box mt-5 mx-auto">
                     <i class="fas fa-4x fa-gem text-primary mb-3 sr-icon-1"></i>
                     <h3 class="mb-4">Experiência 1</h3>
-                    <p class="text-muted mb-2">CRECI – Conselho Regional De Corretores De Imóveis – 13 Região </p>
+                    <p class="text-muted mb-2">Projeto projeto </p>
                     <p class="text-muted mb-2">Duração: 11 meses</p>
                     <p class="text-muted mb-0">Migração de um pequeno sistema feito em Java para um Sistema PHP – Laravel
                         e atividades de desenvolvimento de novas funcionalidades e manutenção de código.</p>
